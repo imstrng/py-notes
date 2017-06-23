3c37c79b8036ad46390f031d7196bf243661b03a
imstrng <imstrng@users.noreply.github.com>
2017-06-23 17:06:09 +0200

    Update asyncio_7.py

--- asyncio-stuff/asyncio_7.py
+++ asyncio-stuff/asyncio_7.py
@@ -8,8 +8,9 @@ mon = {}
 
 store_path='store'
 
-jsonheader = """HTTP/1.0 200 OK
+header = """HTTP/1.0 200 OK
 Content-Type: text/html
+
 """
 
 def loadSettings():
@@ -31,9 +32,34 @@ def saveSettings(mon):
     except:
         pass
 
+
 def store(topic, now, key, val):
     with open(store_path+'/'+now[:10]+'_'+topic+'.log', 'a') as f:
             f.write(now+' '+key+' '+val+'\n')
+
+
+def itemInit(topic):
+    if topic not in mon:
+        mon.update({topic : {'enable':'0', 'start':'', 'end':'', 'snooze': '', 'interval':'', 'last':'', 'status':'', 'descr':'','timeout':'', 'reporter':''}})
+
+
+def itemUpdateStatus(topic, now, key, val, reporter):
+    itemInit(topic)
+    mon[topic].update({'last': now, 'status': key, 'desc': val, 'reporter': reporter})
+    store(topic, now, key, val)
+
+
+def itemUpdateParams(topic, now, key, val):
+    itemInit(topic)
+    mon[topic].update({key: val})
+    store(topic, now, key, val)
+
+
+def getAlerts():
+    out = mon
+    return out
+
+
 
 class Server(asyncio.Protocol):
 
@@ -52,31 +78,41 @@ class Server(asyncio.Protocol):
             return
 
         if self.key in ['ERROR','WARN','OK']:
-            if self.topic not in mon:
-                mon.update({self.topic : {'enable':'0', 'start':'', 'end':'', 'snooze': '', 'interval':'', 'last':'', 'status':'', 'descr':'','timeout':'', 'reporter':''}})
-            mon[self.topic].update({'last': self.now, 'status': self.key, 'desc': self.val, 'reporter': self.reporter})
-            store(self.topic, self.now, self.key, self.val)
+            itemUpdateStatus(self.topic, self.now, self.key, self.val, self.reporter)
 
         elif self.key in ['enable','start','end','snooze','interval','timeout']:
-            print(self.topic,self.key,':',self.val)
-            if self.topic not in mon:
-                mon.update({self.topic : {'enable':'0', 'start':'', 'end':'', 'snooze': '', 'interval':'', 'last':'', 'status':'', 'descr':'','timeout':'', 'reporter':''}})
-            mon[self.topic].update({self.key: self.val})
-            store(self.topic, self.now, self.key, self.val)
+            itemUpdateParams(self.topic, self.now, self.key, self.val)
 
-        elif self.topic == 'admin':
-            if self.val == '1':
-                if self.key == 'save':
-                    saveSettings(mon)
-                if self.key == 'load':
-                    mon = loadSettings()
-                if self.key == 'shutdown':
-                    saveSettings(mon)
-                    print('Shutting down')
-                    exit()
+        elif self.topic in ['GET']:
+            if self.key == '/':
+                self.out = getAlerts()
+                self.out = header + json.dumps(mon, indent=4, sort_keys=True) + '\n'
+                self.transport.write(self.out.encode())
+            else:
+                try:
+                    self.topic, self.key, self.val  = self.key[1:].split('/', 2)
+                except:
+                    self.out = header + json.dumps({'status': 'error'}, indent=4, sort_keys=True) + '\n'
+                    self.transport.write(self.out.encode())
+                    self.transport.close()
+                    return
 
-        elif self.topic == 'GET':
-            print(self.val)
+                if self.topic in mon:
+                    if self.key in ['enable','start','end','snooze','interval','timeout']:
+                        itemUpdateParams(self.topic, self.now, self.key, self.val)
+                        self.out = header + json.dumps({'status': 'ok'}, indent=4, sort_keys=True) + '\n'
+                        self.transport.write(self.out.encode())
+                    
+                    elif self.key in ['enable','start','end','snooze','interval','timeout']:
+                        itemUpdateParams(self.topic, self.now, self.key, self.val)
+                        self.out = header + json.dumps({'status': 'ok'}, indent=4, sort_keys=True) + '\n'
+                        self.transport.write(self.out.encode())
+                    else:
+                        self.out = header + json.dumps({'status': 'error'}, indent=4, sort_keys=True) + '\n'
+                        self.transport.write(self.out.encode())
+                else:
+                    self.out = header + json.dumps({'status': 'error'}, indent=4, sort_keys=True) + '\n'
+                    self.transport.write(self.out.encode())
 
         self.transport.close()
 
@@ -94,3 +130,25 @@ if __name__ == '__main__':
     server.close()
     loop.run_until_complete(server.wait_closed())
     loop.close()
+
+
+'''
+<html>
+<body>
+<table border=1>
+<?php
+$json = file_get_contents('http://amam1w211:5000');
+$json = json_decode($json, true);
+foreach ($json as $k => $v){
+   echo '<tr>';
+   echo '<td>'.$k.'</td>';
+   echo '<td>'.$v['status'].'</td>';
+   echo '<td>'.$v['last'].'</td>';
+   echo '</tr>';
+}
+?>
+</table>
+<pre><?php #print_r($json['nl.aap.test1']); ?></pre>
+</body>
+</html>
+'''
